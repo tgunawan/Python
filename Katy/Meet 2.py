9d7039a1dcf24c8273b6314ca135087a421e1495
Tedi Gunawan <Tedi250415@outlook.com>
2025-01-27 20:17:38 +0700

    update materi

--- Katy/Meet 2.py
+++
@@ -1,5 +0,0 @@
-# Biodata fstring
-# warna
-# conditional
-
-
---
+++ Katy/Project Materi.py
@@ -0,0 +1,14 @@
+# Meet 2
+# Biodata fstring
+# warna
+# conditional
+
+# Meet 3
+# Review
+# Conditional 
+# Simple project implementasi Login Form
+
+# Meet 4
+# Review Conditional
+# simple Calc
+# into loop
