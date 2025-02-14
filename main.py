c372e91ce144d11c60b3bd45e46d802d2d3c5db9
tgunawan <tedi1988@gmail.com>
2025-02-14 18:01:26 +0700

    addfiles

---
+++ main.py
@@ -0,0 +1,11 @@
+# Simple Input/Output Python Code
+import os
+os.system('clear')
+# Ask the user for their name
+name = input("Enter your name: ")
+
+# Ask the user for their age
+age = input("Enter your age: ")
+
+# Print a greeting message
+print(f"Hello, {name}! You are {age} years old.")
