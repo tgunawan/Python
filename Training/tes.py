1eef649c020a7bc5d4bfa3ecca3f1c47a3c35b3c
tgunawan <tedi1988@gmail.com>
2026-05-06 14:52:15 +0700

    update

--- Training/tes.py
+++ Training/tes.py
@@ -1,4 +1,24 @@
-nama = input("Inputkan nama: ")
+import numpy as np
+import pandas as pd
 
-for i in range(3):
-    print(*[nama] * 5)
+np.random.seed(42)
+
+umur = np.random.randint(18, 60, 100)
+income = np.random.randint(3, 15, 100) * 1_000_000
+spending = np.random.randint(1, 100, 100)
+
+df = pd.DataFrame({
+    "Umur": umur,
+    "Income": income,
+    "Spending": spending
+})
+
+print(df.head())
+
+X = df[["Umur", "Income", "Spending"]]
+
+from sklearn.preprocessing import StandardScaler
+
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+
