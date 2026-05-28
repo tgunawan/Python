bf70db4ee92859a2a0a767ae51d8936be737ed33
tgunawan <tedi1988@gmail.com>
2026-05-28 11:17:04 +0700

    decorator

--- tesnumberinput.py
+++ tesnumberinput.py
@@ -1,11 +1,38 @@
-import os
+'''# decorator
+def tampilduluan(func):
+    def ngomong(*args, **kwargs):
+        print("Hallo mulai")
+        return func(*args, **kwargs)
+    return ngomong
+        
+@tampilduluan
+def penutupan():
+    print("Terimakasih sudah hadir")
+    
+class speechpembuka:
+    def __init__(self,nama):
+        self.nama=nama
+        pass
 
-while True:
-    os.system('cls')
-    number=int(input('Masukkan angka: '))
+    @tampilduluan
+    def pembukaan(self):
+        print("Selamat datang di pertemuan ini")
 
-    for i in range(number):
-        for j in range(i+1):
-            print(f'{i+1}',end='')
-        print()
-    input()
+
+pembicara=speechpembuka("test")
+
+pembicara.pembukaan()
+penutupan()'''
+
+# decorator jika funcsi tanpa parameter
+def tampilduluan(func):
+    def ngomong():
+        print("Hallo mulai")
+        return func()
+    return ngomong
+        
+@tampilduluan
+def penutupan():
+    print("Terimakasih sudah hadir")
+
+penutupan()
